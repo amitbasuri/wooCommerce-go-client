28d385c2aec5f69f58cc6adf76bd56f3f866d839
Ayush <ayush@coffeebeans.io>
2020-09-17 23:41:39 +0530

    Ayush : Add createOrder functionality

--- order.go
+++ order.go
@@ -9,22 +9,27 @@ import (
 )
 
 type Order struct {
-	ID              int             `json:"id"`
-	ParentID        int             `json:"parent_id"`
-	Number          string          `json:"number"`
-	OrderKey        string          `json:"order_key"`
-	Status          OrderStatus     `json:"status"`
-	Total           string          `json:"total"`
-	LineItems       []LineItem      `json:"line_items"`
-	TransactionID   string          `json:"transaction_id"`
-	BillingAddress  Address         `json:"billing"`
-	ShippingAddress ShippingAddress `json:"shipping"`
-	Currency        string          `json:"currency"`
-	CreatedAtGMT    string          `json:"date_created_gmt"`
-	DiscountTotal   string          `json:"discount_total"`
-	ShippingTotal   string          `json:"shipping_total"`
-	TotalTax        string          `json:"total_tax"`
-	MetaDataList    []MetaData      `json:"meta_data"`
+	ID                 int             `json:"id"`
+	ParentID           int             `json:"parent_id"`
+	Number             string          `json:"number"`
+	OrderKey           string          `json:"order_key"`
+	Status             OrderStatus     `json:"status,omitempty"`
+	Total              string          `json:"total"`
+	LineItems          []LineItem      `json:"line_items"`
+	TransactionID      string          `json:"transaction_id"`
+	BillingAddress     Address         `json:"billing"`
+	ShippingAddress    ShippingAddress `json:"shipping"`
+	Currency           string          `json:"currency,omitempty"`
+	CreatedAtGMT       string          `json:"date_created_gmt"`
+	DiscountTotal      string          `json:"discount_total"`
+	ShippingTotal      string          `json:"shipping_total"`
+	TotalTax           string          `json:"total_tax"`
+	MetaDataList       []MetaData      `json:"meta_data"`
+	PaymentMethod      string          `json:"payment_method"`
+	PaymentMethodTitle string          `json:"payment_method_title"`
+	SetPaid            bool            `json:"set_paid"`
+	TaxLines           []TaxLines      `json:"tax_lines,omitempty"`
+	ShippingLines      []ShippingLines `json:"shipping_lines,omitempty"`
 }
 
 type MetaData struct {
@@ -75,6 +80,27 @@ type ShippingAddress struct {
 	Phone string `json:"phone"`
 }
 
+type TaxLines struct {
+	Compound         bool          `json:"compound"`
+	ID               int64         `json:"id"`
+	RateID           int64         `json:"rate_id"`
+	RateCode         string        `json:"rate_code"`
+	Label            string        `json:"label"`
+	TaxTotal         string        `json:"tax_total"`
+	ShippingTaxTotal string        `json:"shipping_tax_total"`
+	MetaData         []interface{} `json:"meta_data"`
+}
+
+type ShippingLines struct {
+	ID          int64         `json:"id"`
+	MethodTitle string        `json:"method_title"`
+	MethodID    string        `json:"method_id"`
+	Total       string        `json:"total"`
+	TotalTax    string        `json:"total_tax"`
+	Taxes       []interface{} `json:"taxes"`
+	MetaData    []interface{} `json:"meta_data"`
+}
+
 const OrdersEndpoint = "orders"
 
 func (c *Client) GetOrder(id int) (*Order, error) {
@@ -111,41 +137,40 @@ func (c *Client) GetOrder(id int) (*Order, error) {
 	return &order, nil
 }
 
-//func (c *Client) CreateOrder(o *Order) (*Order, error) {
-//	params, err := json.Marshal(o)
-//	if err == nil {
-//		return nil, err
-//	}
-//	res, err := c.Post(OrdersEndpoint, string(params))
-//	if err != nil {
-//		return nil, NewError(err, http.StatusInternalServerError)
-//	}
-//
-//	defer res.Body.Close()
-//	bodyBytes, err := ioutil.ReadAll(res.Body)
-//	if err != nil {
-//		return nil, NewError(err, http.StatusInternalServerError)
-//	}
-//
-//	if res.StatusCode != http.StatusOK {
-//		var resErr Error
-//		err = json.Unmarshal(bodyBytes, &resErr)
-//		if err != nil {
-//			return nil, NewError(err, res.StatusCode)
-//		}
-//		return nil, NewError(resErr, res.StatusCode, resErr.Message)
-//	}
-//
-//	var products []Product
-//	err = json.Unmarshal(bodyBytes, &products)
-//	if err != nil {
-//		return nil, NewError(err, http.StatusInternalServerError, err.Error())
-//	}
-//
-//	_ = &QueryProductsResponse{
-//		Products: products,
-//		NextPage: nextPage(res.Header, c.NextQueryPageRegexp),
-//	}
-//	return nil, nil
-//
-//}
+func (c *Client) CreateOrder(o *Order) (*Order, error) {
+
+	var orderResponse Order
+
+	params, err := json.Marshal(o)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := c.Post(OrdersEndpoint, string(params))
+	if err != nil {
+		return nil, NewError(err, http.StatusInternalServerError)
+	}
+
+	defer res.Body.Close()
+	bodyBytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, NewError(err, http.StatusInternalServerError)
+	}
+
+	fmt.Println(res.Status)
+
+	if res.StatusCode != http.StatusCreated {
+		var resErr Error
+		err = json.Unmarshal(bodyBytes, &resErr)
+		if err != nil {
+			return nil, NewError(err, res.StatusCode)
+		}
+		return nil, NewError(resErr, res.StatusCode, resErr.Message)
+	}
+
+	if err := json.Unmarshal(bodyBytes, &orderResponse); err != nil {
+		return nil, NewError(err, http.StatusInternalServerError, "error unmarshalling order response")
+	}
+
+	return &orderResponse, nil
+}
